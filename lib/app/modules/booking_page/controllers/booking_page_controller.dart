65688dfbac91ce8cec2d2e2c06f61603590c558c
Rasel Khan <raselk.ptl21@gmail.com>
2023-05-20 17:52:11 +0600

    update seats

--- lib/app/modules/booking_page/controllers/booking_page_controller.dart
+++ lib/app/modules/booking_page/controllers/booking_page_controller.dart
@@ -1,9 +1,38 @@
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../global/global_alert/global_snackbar.dart';
+import '../../../data/services/repository.dart';
 
 class BookingPageController extends GetxController {
-  //TODO: Implement BookingPageController
+  var data = Get.arguments['data'];
+  var tripId = Get.arguments['id'];
 
-  final count = 0.obs;
+  Map<String, TextEditingController> inputs = {
+    "payment": TextEditingController(),
+    "name": TextEditingController(),
+    "phone": TextEditingController(),
+  };
+
+  var isSubmitting = false.obs;
+  submitQuickBook() async {
+    isSubmitting(true);
+    try {
+      String seats = data['seats'].replaceAll("[", '').replaceAll("]", "");
+      seats = seats.substring(0, seats.length - 1);
+
+      Repository().submitQuick(tripId, data).then((value) {
+        if (value['type'] == "success") {
+          GlobalSnackbar.success(msg: value['message']);
+          // Get.offNamed(Routes.TICKET_DETAILS_PAGE,
+          //     arguments: {"id": value['data']['id']});
+        } else {
+          GlobalSnackbar.error(msg: value['message']);
+        }
+      });
+    } on Exception catch (e) {}
+  }
+
   @override
   void onInit() {
     super.onInit();
@@ -19,5 +48,4 @@ class BookingPageController extends GetxController {
     super.onClose();
   }
 
-  void increment() => count.value++;
 }
--- lib/app/modules/booking_page/views/booking_page_view.dart
+++ lib/app/modules/booking_page/views/booking_page_view.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../../core/extra/app_assets.dart';
+import '../../../../core/extra/app_dimens.dart';
+import '../../../../global/global_alert/global_snackbar.dart';
+import '../../../../global/widget/global_btn.dart';
+import '../../../../global/widget/global_input.dart';
+import '../../../../global/widget/global_loading.dart';
 import '../controllers/booking_page_controller.dart';
 
 class BookingPageView extends GetView<BookingPageController> {
@@ -10,13 +16,71 @@ class BookingPageView extends GetView<BookingPageController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('BookingPageView'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Get.back();
+          },
+        ),
+        title: Image.asset(
+          AppAssets.MAIN_LOGO,
+          height: 30,
+          width: Get.width / 2,
+        ),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'BookingPageView is working',
-          style: TextStyle(fontSize: 20),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(24),
+          padding: EdgeInsets.all(12),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade200,
+                  blurRadius: 6,
+                  offset: Offset.zero,
+                  spreadRadius: 2,
+                )
+              ],
+              borderRadius: BorderRadius.circular(6)),
+          child: Column(children: [
+            Text("STAND UP TICKET BOOKING"),
+            Divider(
+              height: 16,
+              thickness: .75,
+              color: Colors.grey.shade400,
+            ),
+            GlobalInput.inputField(
+                controller: controller.inputs['payment']!,
+                headerText: "Payment"),
+            GlobalInput.inputField(
+                controller: controller.inputs['name']!,
+                headerText: "Name (Optional)"),
+            GlobalInput.inputField(
+                controller: controller.inputs['phone']!,
+                headerText: "Phone (Optional)"),
+            SizedBox(
+              height: AppDimens.paddingExtraGaint,
+            ),
+            Obx(
+              () => controller.isSubmitting.value
+                  ? GlobalLoadng.loadingOnly(
+                      color: Theme.of(context).colorScheme.primary)
+                  : GlobalButton.btn(
+                      text: "Buy",
+                      onTap: () {
+                        if (controller.inputs['payment']!.text.isEmpty) {
+                          GlobalSnackbar.error(msg: "Enter payment amount");
+                        } else {
+                          controller.submitQuickBook();
+                        }
+                      }),
+            ),
+            SizedBox(
+              height: AppDimens.breatingSpace,
+            ),
+          ]),
         ),
       ),
     );
